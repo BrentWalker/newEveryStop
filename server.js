371db7589fdf0e9feb0cd6bdf400d62ebdaebf24
Brent Walker <brentwalker@Brents-MacBook-Pro.local>
2019-05-21 17:04:36 -0400

    commit message

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
+
+app.use(express.static(__dirname + '/client/build/'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
